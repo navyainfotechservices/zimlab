02e472a20bacfe18119cacc38b511627d0a5af63
navyainfotechservices <navyainfotechservices@gmail.com>
2018-06-03 04:55:00 +0530

    finally modal done

--- admin/controllers/main.js
+++ admin/controllers/main.js
@@ -7,25 +7,43 @@
  * Controller of the zimlabApp
  */
 angular.module('zimlabApp')
-  .controller('MainCtrl', function ($scope, NavigationService) {
+  .controller('MainCtrl', function ($scope, NavigationService, $state, $modal) {
     $scope.text = "add it bro";
 
     $scope.pages = [];
 
-    $scope.getPagesList = function () {
-      NavigationService.getPagesList({}, function (data) {
-        if (data.statusCode == 0) {
-          $scope.pages = data.result;
-          console.log("$scope.pages", $scope.pages);
-          $state.reload();
-        } else {
-          alert("unable to get pages , please try again");
-        }
-      });
-    }
-    $scope.getPagesList();
+    // $scope.getPagesList = function () {
+    //   NavigationService.getPagesList({}, function (data) {
+    //     if (data.statusCode == 0) {
+    //       $scope.pages = data.result;
+    //       console.log("$scope.pages", $scope.pages);
+    //       $state.reload();
+    //     } else {
+    //       alert("unable to get pages , please try again");
+    //     }
+    //   });
+    // }
+    // $scope.getPagesList();
 
     $scope.addPages = function () {
-      $scope.pages.push({});
-    }
+      // $scope.pages.push({});
+      $scope.items = ['item1', 'item2', 'item3'];
+      var modalInstance = $modal.open({
+        templateUrl: 'views/ui-elements/test.html',
+        controller: 'MainCtrl',
+        size: 'lg',
+        resolve: {
+          items: function () {
+            return $scope.items;
+          }
+        }
+      });
+
+      modalInstance.result.then(function (selectedItem) {
+        $scope.selected = selectedItem;
+      }, function () {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+
   });
