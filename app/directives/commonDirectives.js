e9d18b6c2cb354df5b5c2bda9592de3d01809676
navyainfotechservices <navyainfotechservices@gmail.com>
2018-06-13 23:28:17 +0530

    zimlab

--- app/directives/commonDirectives.js
+++ app/directives/commonDirectives.js
@@ -8,7 +8,7 @@
  */
 angular.module('zimlabApp')
     // Directive for pie charts, pass in title and data only    
-    .directive('hcPieChart', function () {
+    .directive('hcPieChart', function ($timeout) {
         return {
             restrict: 'E',
             template: '<div></div>',
@@ -17,37 +17,40 @@ angular.module('zimlabApp')
                 data: '='
             },
             link: function (scope, element) {
-                Highcharts.chart(element[0], {
-                    chart: {
-                        plotBackgroundColor: null,
-                        plotBorderWidth: null,
-                        plotShadow: false,
-                        type: 'pie',
-                        width:1105
-                    },
-                    title: {
-                        text: scope.title
-                    },
-                    tooltip: {
-                        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-                    },
-                    plotOptions: {
-                        pie: {
-                            allowPointSelect: true,
-                            cursor: 'pointer',
-                            dataLabels: {
-                                enabled: true,
-                                format: '<b>{point.name}</b>: {point.percentage:.1f} %'
-                            },
-                            style: {
-                                color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                $timeout(function () {
+                    Highcharts.chart(element[0], {
+                        chart: {
+                            plotBackgroundColor: null,
+                            plotBorderWidth: null,
+                            plotShadow: false,
+                            type: 'pie',
+                            width: 1105
+                        },
+                        title: {
+                            text: scope.title
+                        },
+                        tooltip: {
+                            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+                        },
+                        plotOptions: {
+                            pie: {
+                                allowPointSelect: true,
+                                cursor: 'pointer',
+                                dataLabels: {
+                                    enabled: true,
+                                    format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+                                },
+                                style: {
+                                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                                }
                             }
-                        }
-                    },
-                    series: [{
-                        data: scope.data
-                    }]
-                });
+                        },
+                        series: [{
+                            data: scope.data
+                        }]
+                    });
+                }, 10000);
+
             }
         };
     });
